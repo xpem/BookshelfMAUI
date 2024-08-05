40c911b5632c6a52651684ddea31f1555850f905
Emanuel <emanuel.xpe@gmail.com>
2024-08-05 18:29:10 -0300

    criação de exceção propria para a comunicação com a api com um retorno. Closes #58

---
+++ Models/Exceptions/BookshelfAPIException.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.Exceptions
+{
+    [Serializable]
+    public class BookshelfAPIException : Exception
+    {
+        public BookshelfAPIException() { }
+
+        public BookshelfAPIException(string message) : base(message) { }
+    }
+}
